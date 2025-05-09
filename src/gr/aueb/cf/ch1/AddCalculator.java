fb58584ce8cd492e3690185bc46ca08354da522a
Manos Stergios <manostergios97@gmail.com>
2025-05-09 18:28:57 +0300

    project created

---
+++ src/gr/aueb/cf/ch1/AddCalculator.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.ch1;
+
+public class AddCalculator {
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int nu2 = 20;
+        int result = 0;
+
+        result = add(num1, nu2);
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
